d2b2e8056fdda33724af22629ca8ad52b7a54036
Alex Smith <alex.smith2844@gmail.com>
2020-06-02 17:04:54 +0500

    support colab

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
-URL="https://raw.githubusercontent.com/alex2844/youtube-music/master/ymusic.py";
-fetch() {
-	$(echo $([ `type -p curl` ] && echo "curl -s" || echo "wget -q -O -")" $1");
-}
 if [[ ! -z `type -p apt` ]]; then
 	if [[ -z `type -p python3` || -z `type -p pip3` || -z `type -p ffmpeg` ]]; then
 		sudo apt install python3 python3-pip ffmpeg
@@ -11,6 +7,6 @@ if ! (stat -t /usr/local/lib/python3*/dist-packages/{ytmusicapi,youtube_dl,mutag
 	sudo pip3 install ytmusicapi youtube-dl mutagen
 fi
 if ! (stat -t /usr/local/lib/python*/dist-packages/google/colab > /dev/null 2>&1) then
-	fetch "$URL" > /usr/local/bin/ymusic;
+	$(echo $([ `type -p curl` ] && echo "curl -s" || echo "wget -q -O -")" https://raw.githubusercontent.com/alex2844/youtube-music/master/ymusic.py") > 'test.py'
 	chmod +x /usr/local/bin/ymusic;
 fi
