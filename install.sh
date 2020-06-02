6cb7e1145511f153128ac27544a3d0b4e40e1622
Alex Smith <alex.smith2844@gmail.com>
2020-06-02 17:10:10 +0500

    support colab

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 if [[ ! -z `type -p apt` ]]; then
 	if [[ -z `type -p python3` || -z `type -p pip3` || -z `type -p ffmpeg` ]]; then
-		sudo apt install python3 python3-pip ffmpeg
+		apt install python3 python3-pip ffmpeg
 	fi
 fi
 if ! (stat -t /usr/local/lib/python3*/dist-packages/{ytmusicapi,youtube_dl,mutagen} > /dev/null 2>&1) then
-	sudo pip3 install ytmusicapi youtube-dl mutagen
+	pip3 install ytmusicapi youtube-dl mutagen
 fi
 if ! (stat -t /usr/local/lib/python*/dist-packages/google/colab > /dev/null 2>&1) then
-	sudo $(echo $([ `type -p curl` ] && echo "curl -s" || echo "wget -q -O -")" https://raw.githubusercontent.com/alex2844/youtube-music/master/ymusic.py") > '/usr/local/bin/ymusic'
-	sudo chmod +x /usr/local/bin/ymusic;
+	$(echo $([ `type -p curl` ] && echo "curl -s" || echo "wget -q -O -")" https://raw.githubusercontent.com/alex2844/youtube-music/master/ymusic.py") > '/usr/local/bin/ymusic'
+	chmod +x /usr/local/bin/ymusic;
 fi
