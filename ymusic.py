5a1455c9c05ed7525963a0aae66afc1fe39c5d12
dariy-vel <dariy.velychko@gmail.com>
2020-10-04 13:01:12 +0300

    Fixed syntax warning

--- ymusic.py
+++ ymusic.py
@@ -145,7 +145,7 @@ def main(args):
     opt = ['help', 'version', 'doubles', 'skip-error', 'colab', 'auth',
            'all', 'one=', 'playlist=', 'sync', 'no-subfolder', 'output=']
     arguments, values = getopt.getopt(args, 'hvdao:p:s', opt)
-    if len(arguments) is 0:
+    if len(arguments) == 0:
         if os.environ.get('COLAB_GPU', False):
             arguments = [('--colab', '')]
         else:
