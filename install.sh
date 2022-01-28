f130eaebdf8b84cff375bdafeb44b34df5408825
Alex Smith <alex.smith2844@gmail.com>
2022-01-28 20:47:29 +0500

    'autoinstall dependencies'

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 if [[ ! -z `type -p apt` ]]; then
 	if [[ -z `type -p python3` || -z `type -p pip3` || -z `type -p ffmpeg` ]]; then
-		apt install python3 python3-pip ffmpeg
+		apt install python3 python3-pip ffmpeg -y
 	fi
 fi
 if ! (stat -t /usr/local/lib/python3*/dist-packages/{ytmusicapi,youtube_dl,mutagen} > /dev/null 2>&1) then
